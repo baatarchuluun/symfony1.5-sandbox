746da2a658637f8416106138ecf83757253bdb72
Baatarchuluun Altanbayar <baatarchuluuna@gmail.com>
2020-08-25 13:18:41 +0800

    refactor: baseUri in app.yml and added template

--- apps/frontend/modules/public/templates/indexSuccess.php
+++ apps/frontend/modules/public/templates/indexSuccess.php
@@ -1,2 +1,10 @@
-<h1>Hello World</h1>
+<h1>Oauth login via Greensoft account</h1>
 
+<?php if ($sf_user->isAuthenticated()): ?>
+    FirstName: <?php echo $user['firstName']; ?> <br/>
+    LastName: <?php echo $user['lastName']; ?><br/>
+    UserId: <?php echo $user['id']; ?><br/>
+    Email address: <?php echo $user['email']; ?>
+<?php else: ?>
+    <a href="<?php echo sfConfig::get('app_oauth_login_uri'); ?>">Login</a>
+<?php endif; ?>
